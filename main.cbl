1c66dc9eb497bb54cc86db67248f65492b70735e
theluqmn <luqman.yazrie@outlook.com>
2025-08-17 06:37:29 +0800

    ulalalaa

--- main.cbl
+++ main.cbl
@@ -3,18 +3,28 @@
        PROGRAM-ID. WAREBALL.
        AUTHOR. theluqmn.
 
-       ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 MENU-INPUT PIC X.
 
        PROCEDURE DIVISION.
-       UI-MENU.
-           DISPLAY "wareball".
-           DISPLAY "[0] quit".
-           ACCEPT MENU-INPUT.
+      *processing the menu input to call functions
+       MENU-PROCESS.
+           IF MENU-INPUT = "1" THEN
+               DISPLAY "1".
 
-       PERFORM UI-MENU UNTIL MENU-INPUT = 0.
+      *display the menu
+       MENU-DISPLAY.
+           DISPLAY "WAREBALL".
+           DISPLAY "inventory done correctly".
+           DISPLAY "".
+           DISPLAY "[0] quit program".
+           ACCEPT MENU-INPUT.
+           PERFORM MENU-PROCESS.
+
+      *main program loop
+       PERFORM MENU-DISPLAY UNTIL MENU-INPUT = 0.
+       DISPLAY "exiting WAREBALL..."
        STOP RUN.
        
        END PROGRAM WAREBALL.
