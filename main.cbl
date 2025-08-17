2eadcdbaeb8b3596a13f000891ac2f3618edf277
theluqmn <luqman.yazrie@outlook.com>
2025-08-17 11:36:44 +0800

    organised some stuff

--- main.cbl
+++ main.cbl
@@ -5,7 +5,7 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 MENU-INPUT PIC X.
+       01 MENU-INPUT PIC X(10).
 
        01 ITEM-DATA.
            05 ITEM-ID PIC X(20).
@@ -21,36 +21,49 @@
                10 FILLER PIC 9(5)V99.
 
        PROCEDURE DIVISION.
+       DISPLAY "------------------------------------------".
+       DISPLAY "WAREBALL".
+       DISPLAY "inventory done correctly".
+       DISPLAY "run 'help' for list of available commands.".
        PERFORM MAIN-PROCEDURE.
 
       *processing the menu input to call functions
        MENU-PROCESS.
-           IF MENU-INPUT = "0" THEN
+           IF MENU-INPUT = "help" THEN
+               PERFORM MENU-HELP.
+           IF MENU-INPUT = "quit" THEN
                PERFORM MAIN-END.
-           IF MENU-INPUT = "1" THEN
+           IF MENU-INPUT = "add" THEN
                PERFORM OPERATION-ADD.
+           IF MENU-INPUT = "update" THEN
+               PERFORM OPERATION-UPDATE.
+           IF MENU-INPUT = "delete" THEN
+               PERFORM OPERATION-DELETE.
+           IF MENU-INPUT = "report" THEN
+               PERFORM OPERATION-REPORT.
 
       *display the menu
        MENU-DISPLAY.
-           DISPLAY "------------------------".
-           DISPLAY "WAREBALL".
-           DISPLAY "inventory done correctly".
-           DISPLAY "------------------------".
-           DISPLAY "[1] add item".
-           DISPLAY "[2] update item".
-           DISPLAY "[3] delete item".
-           DISPLAY "[4] generate report".
-           DISPLAY "-".
-           DISPLAY "[0] quit program".
-           DISPLAY " ".
+           DISPLAY "------------------------------------------".
            ACCEPT MENU-INPUT.
            PERFORM MENU-PROCESS.
+       
+       MENU-HELP.
+           DISPLAY "------------------------------------------".
+           DISPLAY "LIST OF COMMANDS".
+           DISPLAY "------------------------------------------".
+           DISPLAY "[add]      add an item".
+           DISPLAY "[update]   update an item's quantity".
+           DISPLAY "[delete]   delete an item".
+           DISPLAY "[report]   generate a report".
+           DISPLAY "-".
+           DISPLAY "[quit]     quit wareball".
 
       *operation to add new item
        OPERATION-ADD.
-           DISPLAY "------------------------".
+           DISPLAY "------------------------------------------".
            DISPLAY "ADD A NEW ITEM".
-           DISPLAY "------------------------".
+           DISPLAY "------------------------------------------".
            DISPLAY "(1/4) ID:".
            ACCEPT ITEM-ID.
            DISPLAY "(2/4) description:".
@@ -59,21 +72,36 @@
            ACCEPT ITEM-QUANTITY.
            DISPLAY "(4/4) price:".
            ACCEPT ITEM-PRICE.
+
+           DISPLAY " ".
+           DISPLAY "item added successfully".
        
        OPERATION-UPDATE.
-           DISPLAY "------------------------".
+           DISPLAY "------------------------------------------".
            DISPLAY "UPDATE AN ITEM".
-           DISPLAY "------------------------".
+           DISPLAY "------------------------------------------".
+           DISPLAY "(1/2) ID:".
+           ACCEPT ITEM-ID.
+           DISPLAY "(2/2) quantity:".
+           ACCEPT ITEM-QUANTITY.
+
+           DISPLAY " ".
+           DISPLAY "item updated successfully".
        
        OPERATION-DELETE.
-           DISPLAY "------------------------".
+           DISPLAY "------------------------------------------".
            DISPLAY "DELETE AN ITEM".
-           DISPLAY "------------------------".
+           DISPLAY "------------------------------------------".
+           DISPLAY "ID:".
+           ACCEPT ITEM-ID.
+
+           DISPLAY " ".
+           DISPLAY "item deleted successfully".
        
        OPERATION-REPORT.
-           DISPLAY "------------------------".
+           DISPLAY "------------------------------------------".
            DISPLAY "GENERATE A NEW REPORT".
-           DISPLAY "------------------------".
+           DISPLAY "------------------------------------------".
 
       *main program loop
        MAIN-PROCEDURE.
